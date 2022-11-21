8df63017e7c0271823643e8a40208d429b8bbec5
Livia <livia@MacBook-Air-de-Livia.local>
2022-11-21 21:57:12 +0100

    list of updates

--- site/js/plot/plot.js
+++ site/js/plot/plot.js
@@ -75,7 +75,7 @@ function plot() {
 
   const mousemove = function(d) {
     Tooltip
-      .html(`${d[0].name}<br>${pcs['x']}: ${d[0].value}<br>${pcs['y']}: ${d[1].value}`)
+      .html(`${d[0].name} <br>${pcs['x']}: ${d[0].value}<br>${pcs['y']}: ${d[1].value}`)
       .style("left", (70) + "px")
       .style("top", (70) + "px")
       .style("left", (d3.mouse(this)[0]+90) + "px")
