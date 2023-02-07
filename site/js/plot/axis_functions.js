108c67ac67a677ab3992e660930b3accc9f3f72c
Livia Rocha <livia.silva.rocha@usp.br>
2023-02-07 13:56:05 -0300

    added feature

--- site/js/plot/axis_functions.js
+++ site/js/plot/axis_functions.js
@@ -26,18 +26,62 @@ const defaultAxis = (x, y, width, height, margin, svg, XAxisLabel, YAxisLabel) =
     .call(d3.axisLeft(y_axis)
     .ticks( innerHeight/ 60))
     .attr('opacity', '0');
+  
 
   // Add X axis label
+  /*
+    svg.append("g")
+  .append("foreignObject")
+  .attr("width", width)
+  .attr("height",  height)
+  .attr("x", (width + margin.left + margin.top) / 2)
+  .attr("y", height + 30)
+  .append("xhtml:body")
+  .style("font", "16px")
+  .style("color", 'var(--text-color)')
+  .html("<p>An HTML test</p>");
 
- /* svg.append("text")
+  svg.append("text")
     .style('fill', 'white')
     .style('font-size', '25px')
     .attr("text-anchor", "middle")
     .attr("x", width / 2)
     .attr("y", height + margin.top + 37)
-    .text(XAxisLabel);*/
+    .text(XAxisLabel);
+ 
+  */
+
+  var latex_raw = "\\text{SFR}{[\\textup{O\\ II}]}=10.10\\text{M}{\\odot}\\ {\\text{ yr}}^{-1}";
+  var latex_render_url = "http://latex.codecogs.com/gif.latex?";
+  var latex_query = encodeURI(latex_raw);
+  var latex = svg.append("foreignObject")
+      .attr("x", width / 2 - 80)
+      .attr("y", height + margin.top + 20)
+      .attr("width", 400)
+      .attr("height", 200)
+      .attr("requiredFeatures", "http://www.w3.org/TR/SVG11/feature#Extensibility")
+      .append("xhtml:body")
+      .attr("text-anchor", "middle")
+      .attr("x", width / 2)
+      .attr("y", height + margin.top + 37)
+      .append("img")
+      .style("color", 'var(--text-color)')
+      .attr("src", latex_render_url + latex_query);
 
   // Y axis label
+
+  /*svg.append("g")
+  .append("foreignObject")
+  .attr("width", width)
+  .attr("height",  height)
+  .attr("transform", "rotate(-90)")
+  .attr("y", -margin.left + 25)
+  .attr("x", -margin.top -(height / 2))
+  .append("xhtml:body")
+  .style("font", "16px")
+  .style("color", 'var(--text-color)')
+  .html("<p>An HTML test</p>");
+
   svg.append("text")
     .style('fill', 'white')
     .style('font-size', '25px')
@@ -45,7 +89,25 @@ const defaultAxis = (x, y, width, height, margin, svg, XAxisLabel, YAxisLabel) =
     .attr("transform", "rotate(-90)")
     .attr("y", -margin.left + 25)
     .attr("x", -margin.top -(height / 2))
-    .text(YAxisLabel)
+    .text(YAxisLabel)*/
+
+  var latex_raw = "\\text{SFR}{[\\textup{O\\ II}]}=10.10\\text{M}{\\odot}\\ {\\text{ yr}}^{-1}";
+  var latex_render_url = "http://latex.codecogs.com/gif.latex?";
+  var latex_query = encodeURI(latex_raw);
+  var latex = svg.append("foreignObject")
+    .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left + 25)
+      .attr("x", -margin.top -(height / 2))
+      .attr("width", 400)
+      .attr("height", 200)
+      .attr("requiredFeatures", "http://www.w3.org/TR/SVG11/feature#Extensibility")
+      .append("xhtml:body")
+      .attr("text-anchor", "middle")
+      .attr("y", -margin.left + 25)
+      .attr("x", -margin.top -(height / 2))
+      .style("color", 'var(--text-color)')
+      .append("img")
+      .attr("src", latex_render_url + latex_query);
 
   // X axis animation
   x_axis.domain([
@@ -74,6 +136,7 @@ const defaultAxis = (x, y, width, height, margin, svg, XAxisLabel, YAxisLabel) =
   .ticks(innerHeight/60));
 
   return [x_axis, y_axis];
+
 }
 
 /*
